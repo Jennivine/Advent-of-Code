332d73fde449182f755f6b8dd51ae58fe26475dd
Jennivine <jennivine@live.cn>
2018-12-30 21:04:10 +1100

    Initial commit

---
+++ 2018/Day_1.py
@@ -0,0 +1,35 @@
+#PART ONE
+with open("day1in.txt") as I:
+	frequencies = map(int, I.readlines())
+
+reachedFreq = set()
+ans = 0
+
+for i in frequencies:
+	ans += i
+
+print ans
+
+
+#PART TWO
+import itertools
+ans = 0 #reset ans for second part :)
+SIZE = len(frequencies)
+i = 0
+
+while not ans in reachedFreq:
+	reachedFreq.add(ans)
+	ans += frequencies[i]
+	i = (i+1)%SIZE
+
+print ans
+
+'''
+for num in itertools.cycle(frequencies):
+	ans += num
+	if ans in reachedFreq:
+		print ans
+		break
+	reachedFreq.add(ans)
+
+'''
